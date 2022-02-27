03734e31118670208e4df9ce4c5105698d000450
aimedtuba <cademcnair@aimedtuba.com>
2022-02-27 20:04:43 +0700

    stand with ukraine

--- client/src/Index.vue
+++ client/src/Index.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="lgray-div apps-div">
             <component :is="website_code+'aimedtuba-app'" :code="secure.accounts.codes.website" description="the aimedtuba accounts system, very important (you're logged in)" name="accounts.aimedtuba.com" url="http://$name/signup?code=$code" style="display:inline"></component>
-            <component :is="website_code+'aimedtuba-app'" :code="secure.pictures.codes.website" description="my pictures website, not mobile freindly (50/50 for update)" name="pictures.aimedtuba.com" url="http://$name/?$code=code" style="display:inline"></component>
+            <component :is="website_code+'aimedtuba-app'" :code="secure.pictures.codes.website" description="my pictures website, just got update" name="pictures.aimedtuba.com" url="http://$name/?code=$code" style="display:inline"></component>
             <component :is="website_code+'aimedtuba-app'" :code="'_'" description="pretty simple game that i made, has multiplayer" name="game.aimedtuba.com" url="http://$name" style="display:inline"></component>
             <div class="fivepx-height"></div>
         </div>
@@ -16,7 +16,24 @@
 </template>
 
 <script>
-import secure from 'tuba-secure'
+const secure={
+    main:{
+        mongodb:{
+            url:"mongodb+srv://aimedtuba:mongodbtuba01@aimedtuba.nlry1.mongodb.net",
+        },
+        codes:{
+            website:"i28y8te51p215p4zj5z79s8tsdh2zgesjjwpo8gthf4p2tlfmn6dugfvr13zht1nl9yujamkp3dl8sy98014zf9g78e2ce41y126901",
+            announcements:"D9BZcu925HuImj56N3PR4KLP9XDaDP3NedXOqS5UW6CyO6GkH7j2K7y75w83y1fw994Zs39Fu0Rbj13zbgJQ0E1y0Rm3khhyfx4I",
+        },
+    },
+    pictures:{
+        codes:{website:"ferp7sjNHVn64MRhW2C0H5YKZo66sv0LCNGT9R7kHBXlUHCq6yZ7e41Vs5355jQkSx3mF5x3BUm47sRTuW7MPr7GBdmYu2Au1R3K"}
+    },
+    accounts:{
+        mongodb:{url:"mongodb+srv://aimedtuba:mongodbtuba01@aimedtuba.nlry1.mongodb.net",},
+        codes:{website:"w3TeRDEqKJXvow0Gs12mwywQlR8UJsMDz9C5YRgL6xSGjZ1ZPTW2yC57YdIDJr10t7LF26I4tQyT11z260is3KA7wX9Wn659SLcy"},
+    }
+}
 
 export default {
     data() {
--- client/src/Router.vue
+++ client/src/Router.vue
@@ -3,3 +3,26 @@
         <router-view></router-view>
     </div>
 </template>
+
+<script>
+    export default{
+        mounted(){
+            var SWU__DOMReady = function(callback) {
+                document.readyState === 'interactive' || document.readyState === 'complete' ? callback() : document.addEventListener('DOMContentLoaded', callback);
+            };
+            SWU__DOMReady(function() {
+                const el = document.createElement('div')
+                el.title = 'We stand with Ukraine'
+                el.style.position = 'fixed'
+                el.style.left = '-80px'
+                el.style.bottom = '20px'
+                el.style.width = '300px'
+                el.style.height = '84px'
+                el.style.transform = 'rotate(45deg)'
+                el.style.zIndex = '999'
+                el.style.background = 'linear-gradient(-180deg, #005BBB 50%, #FFD500 50%)'
+                document.body.appendChild(el)
+            });
+        }
+    }
+</script>
